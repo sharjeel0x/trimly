4a6e78818c300ef044eb4ad8d54832a41e96f08b
sharj33l0x <sharj33lshahzad@gmail.com>
2025-11-25 04:47:01 +0500

    Make landing page, install Accordion and input component from shadcn

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -5,16 +5,12 @@ import { Outlet } from "react-router-dom";
 const AppLayout = () => {
   return (
     <div>
-      <main className="min-h-screen container"  >
-
-         <Header />
+      <main className="min-h-screen container">
+        <Header />
         <Outlet />
       </main>
 
-       <div className="p-10 text-center mt-10 bg-gray-800">
-
-         sharjeel0x ©
-       </div>
+      <div className="p-10 text-center mt-10 bg-gray-800">sharjeel0x ©</div>
     </div>
   );
 };
